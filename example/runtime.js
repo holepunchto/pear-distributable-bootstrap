0b2dc7c0f1fe7048f15ace012b47b32f051e2c11
Andrei Regiani <andrei.cpp@gmail.com>
2025-07-24 17:54:30 +0700

    hypercoreid

--- example/runtime.js
+++ example/runtime.js
@@ -1,9 +1,9 @@
-const { decode } = require('hypercore-id-encoding')
+const hypercoreid = require('hypercore-id-encoding')
 const bootstrap = require('..')
 
 console.log('Bootstrapping...')
 bootstrap({
-  pearKey: decode('pqbzjhqyonxprx8hghxexnmctw75mr91ewqw5dxe1zmntfyaddqy'), // production build
+  pearKey: hypercoreid.decode('pqbzjhqyonxprx8hghxexnmctw75mr91ewqw5dxe1zmntfyaddqy'), // production build
   pearDir: __dirname,
   appLink: 'pear://runtime'
 })
