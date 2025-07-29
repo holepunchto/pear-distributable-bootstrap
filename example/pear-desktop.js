fa38e665fffd5824853ddfa672b13238f54323ef
Andrei Regiani <andrei.cpp@gmail.com>
2025-07-30 02:09:09 +0700

    trusted: true, pear://runtime example

--- example/pear-desktop.js
+++ example/pear-desktop.js
@@ -5,5 +5,5 @@ console.log('Bootstrapping...')
 bootstrap({
   pearKey: hypercoreid.decode('dhpc5npmqkansx38uh18h3uwpdp6g9ukozrqyc4irbhwriedyeho'),
   pearDir: __dirname,
-  appLink: 'pear://rbw6fbxorqgjgyitworh3f73utc5cu7sczhptn64oonbznuojiao'
+  appLink: 'pear://rbw6fbxorqgjgyitworh3f73utc5cu7sczhptn64oonbznuojiao' // pear://runtime
 })
